fecc3db926632fc68473fd007f2f877ea8358948
Dhardin44 <dhardin44@ivytech.edu>
2026-04-21 16:29:03 -0500

    Add files via upload

---
+++ my first Python program.py
@@ -0,0 +1,14 @@
+print("Hello World")
+print("I just wrote my first Python program")
+print("This is so much fun")
+name = input("Hello, what is your name?") #get the users name
+print("Welcome, it is so nice to meet you" + name)
+c = ("cats")
+d = ("dogs")
+s = ("It is raining " + c + " and " + d + ".")
+print(s)
+
+
+
+
+
