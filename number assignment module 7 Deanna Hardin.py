936d14244dd64e404c140fdc409d93acaf57093e
Dhardin44 <dhardin44@ivytech.edu>
2026-04-29 19:18:16 -0500

    Add files via upload

---
+++ number assignment module 7 Deanna Hardin.py
@@ -0,0 +1,12 @@
+numbers = [1, 4, 7, 10, 12, 15, 18, 21, 24, 27, 30, 33, 36, 39, 42]
+
+for number in numbers:
+    if number % 2 == 0:
+        print(str(number) + " is even")
+    else:
+        print(str(number) + " is odd")
+        
+
+    
+
+    
