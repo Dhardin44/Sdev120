07c6df171351bfbcf4769b9529afae95fb7300c3
Dhardin44 <dhardin44@ivytech.edu>
2026-05-06 19:03:21 -0500

    Add files via upload

---
+++ module 8 Python.py
@@ -0,0 +1,15 @@
+def greater_than(x, y):
+    return x > y
+    # Function compares two numbers and will return true if x is greater than y.
+
+    
+# My first test scenario
+a = 10
+b = 6
+result = greater_than(a, b)
+
+print("The statement " + str(a) + " is greater than " + str(b) + " is " + str(result))
+
+
+
+
